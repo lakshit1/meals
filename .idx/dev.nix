7cd918927adc168546e16a1f61c796332fb49ae1
agglak2 <51991640+lakshit1@users.noreply.github.com>
2024-07-27 18:37:29 +0000

    customize idx config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -5,28 +5,28 @@
     pkgs.jdk17
     pkgs.unzip
   ];
+  command = ["git config --global user.email \"51991640+lakshit1@users.noreply.github.com\""];
   idx.extensions = [
-
-
     "Dart-Code.dart-code"
     "Dart-Code.flutter"
+    "robert-brunhage.flutter-riverpod-snippets"
   ];
   idx.previews = {
     previews = {
-      web = {
-        command = [
-          "flutter"
-          "run"
-          "--machine"
-          "-d"
-          "web-server"
-          "--web-hostname"
-          "0.0.0.0"
-          "--web-port"
-          "$PORT"
-        ];
-        manager = "flutter";
-      };
+      # web = {
+      #   command = [
+      #     "flutter"
+      #     "run"
+      #     "--machine"
+      #     "-d"
+      #     "web-server"
+      #     "--web-hostname"
+      #     "0.0.0.0"
+      #     "--web-port"
+      #     "$PORT"
+      #   ];
+      #   manager = "flutter";
+      # };
       android = {
         command = [
           "flutter"
